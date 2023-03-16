cc3fcd4eaa874cbd231a61bd3984a2b4affad982
Supreet14 <55839742+Supreet14@users.noreply.github.com>
2023-03-16 11:42:52 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,5 @@
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs
 provider "google" {
-  credentials = "${file("service.json")}"
   project = var.p_id
   region  = var.region
 }
